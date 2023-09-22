04b6f520e4355e3ab8822d9d6d86117fb5e09625
Erika Duarte <erika.duarte.mora@gmail.com>
2023-09-22 10:36:27 -0500

    primer test

--- test/GuessTheNumberGameTest.java
+++ test/GuessTheNumberGameTest.java
@@ -1,5 +1,25 @@
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import java.util.Random;
 import static org.junit.jupiter.api.Assertions.*;
 
-class GuessTheNumberGameTest {
 
+public class GuessTheNumberGameTest {
+   @Test
+   void NumberRandom(){
+
+
+   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
